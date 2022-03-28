b8623dc7a12982552aaa87fe37d05ba60de4df76
Ryan Maloney <rypmaloney@gmail.com>
2022-03-28 19:24:05 -0400

    Created initial app with CRA. Deleted unneeded files and content. Ready to begin.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <header>
+                <h1>Ryan Maloney's Portfolio</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
