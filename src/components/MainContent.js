2990af03749b95b1dbc70c04d010595950b0bcfb
Ryan Maloney <rypmaloney@gmail.com>
2022-04-10 10:42:19 -0400

    removed blog logic

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,31 +6,6 @@ import './MainContent.css';
 import StarField from './StarField/StarField';
 
 const MainContent = (props) => {
-    const [posts, setPosts] = useState(false);
-
-    //This is in this component and not Blogs so that the data is ready and waiting by the time a user clicks on it
-    const getPosts = async () => {
-        try {
-            const res = await fetch(
-                'http://localhost:3000/api/posts/' //'https://obscure-wildwood-18149.herokuapp.com/api/posts/ '
-            );
-            if (res.status !== 200) {
-                console.log(res.status);
-                setPosts(false);
-            } else {
-                const posts = await res.json();
-                setPosts(posts);
-            }
-        } catch (err) {
-            setPosts(false);
-            console.log(err);
-        }
-    };
-
-    useEffect(() => {
-        getPosts();
-    }, []);
-
     if (props.selectedContent === 'projects') {
         return (
             <div className='content'>
